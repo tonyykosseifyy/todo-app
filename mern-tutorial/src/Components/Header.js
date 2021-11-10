5610beeea5a29f187d1f7f6cb526801d46077583
tonyykosseifyy <tonykosseify123@gmail.com>
2021-11-10 21:13:33 +0200

    using styled-components instead of pure css

--- mern-tutorial/src/Components/Header.js
+++ mern-tutorial/src/Components/Header.js
@@ -1,10 +1,9 @@
 import { useState , useEffect } from "react";
-import "./Header.css";
 import bgDesktopLight from "../Assets/bg-desktop-light.jpg";
 import bgDesktopDark from "../Assets/bg-desktop-dark.jpg";
 import bgMobileLight from "../Assets/bg-mobile-light.jpg";
 import bgMobileDark from "../Assets/bg-mobile-dark.jpg";
-
+import styled from "styled-components";
 
 
 const Header = ({ darkTheme , setDarkTheme }) => {
@@ -15,13 +14,24 @@ const Header = ({ darkTheme , setDarkTheme }) => {
   }, [])
   console.log(mobile);
   return (
-    <div className='header'>
+    <HeaderWrapper>
       <img 
       src={mobile ? darkTheme ? bgMobileDark:bgMobileLight : darkTheme ? bgDesktopDark:bgDesktopLight} 
       alt="design image" />
-    </div>
+    </HeaderWrapper>
   );
 };
 
 
 export default Header;
+
+
+const HeaderWrapper = styled.header`
+  height: 300px;
+
+  & > img {
+    width: 100% ;
+    height: 100%;
+    object-fit: cover;
+  }
+`
